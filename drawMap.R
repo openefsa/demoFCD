ca8290b2e4d012f835f2499d685de07866d25268
Carsten Behring <carsten.behring@gmail.com>
2016-04-27 16:24:39 +0200

    added data

--- drawMap.R
+++ drawMap.R
@@ -1,22 +1,26 @@
 library(RColorBrewer)
 library(maptools)
 library(ggplot2)
+library(tmap)
+library(efsagis)
 
-data(wrld_simpl)
+
+data(World)
 eu_countries <- c("AL", "AD", "AT", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK",
-                  "EE", "FO", "FI", "FR", "DE", "GI", "GR", "HU", "IS", "IE", "IT", "LV", "LI", "LT", "LU", "MK", "MT",
-                  "MC", "NL", "NO", "PL", "PT", "RO", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "GB", "VA", "RS", "IM", "RS", "ME")
-
+                 "EE", "FO", "FI", "FR", "DE", "GI", "GR", "HU", "IS", "IE", "IT", "LV", "LI", "LT", "LU", "MK", "MT",
+                 "MC", "NL", "NO", "PL", "PT", "RO", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "GB", "VA", "RS", "IM", "RS", "ME")
 
 
 plotme <- function(ddf,foodName) {
     
 
-    wrld_simpl@data$id <- wrld_simpl@data$NAME
-    europe <- wrld_simpl[wrld_simpl@data$ISO2 %in% eu_countries,]
-    
+    ##wrld_simpl@data$id <- wrld_simpl@data$NAME
+    ##europe <- wrld_simpl[wrld_simpl@data$ISO2 %in% eu_countries,]
+    europe <- World[World@data$continent == "Europe",]
+    europe@data$id <- europe@data$name
+    europe <- europe[!europe@data$name %in% c("Ukraine","Russia"), ]
     europe <- fortify(europe, region="id")
-
+    
     gg <- ggplot() + ggtitle(paste0("Consumption of ",foodName," in Europe"))
     
 
@@ -25,12 +29,12 @@ plotme <- function(ddf,foodName) {
 
     gg <- gg + labs(x="", y="")
     gg <- gg + theme(plot.background = element_rect(fill = "transparent", colour = NA),
-                     panel.border = element_blank(),
-                     panel.background = element_rect(fill = "transparent", colour = NA),
-                     panel.grid = element_blank(),
-                     axis.text = element_blank(),
-                     axis.ticks = element_blank(),
-                     legend.position = "right")
+                    panel.border = element_blank(),
+                    panel.background = element_rect(fill = "transparent", colour = NA),
+                    panel.grid = element_blank(),
+                    axis.text = element_blank(),
+                    axis.ticks = element_blank(),
+                    legend.position = "right")
    
     if(nrow(ddf) == 0) {
         return(gg)
@@ -39,9 +43,9 @@ plotme <- function(ddf,foodName) {
         ddf$value <- jitter(ddf$value+0.1) 
 
         ddf$brk <- cut(ddf$value, 
-                       breaks=c(0, sort(ddf$value)), 
-                       labels=paste0(as.character(ddf[order(ddf$value),]$Country)," - ",round(sort(ddf$value),4)),
-                       include.lowest=TRUE)
+                      breaks=c(0, sort(ddf$value)), 
+                      labels=paste0(as.character(ddf[order(ddf$value),]$Country)," - ",round(sort(ddf$value),4)),
+                      include.lowest=TRUE)
 
                                         # add our colored regions
         gg <- gg + geom_map(data=ddf, map=europe, aes(map_id=Country, fill=brk),  color="#7f7f7f", size=0.25)
@@ -50,7 +54,7 @@ plotme <- function(ddf,foodName) {
         gg <- gg + scale_fill_manual(values=colorRampPalette(brewer.pal(9, 'Reds'))(length(ddf$value)),   name="mean consumptions g/day")
 
                                         # this gives us proper coords. mercator proj is default
-        gg <- gg + coord_map()
+                                        #gg <- gg + coord_map()
         gg
     }
 
